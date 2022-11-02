b06146c099968c530cdc9c37ee43f817e1014393
J4CKINS <electronicjack4@gmail.com>
2022-11-02 00:50:15 +0000

    Created nav bar template

---
+++ templates/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <p id="title" class="clickable">Getaway</p>
+    <ul>
+        <li class="clickable">Home</li>
+        <li class="clickable">Hotels</li>
+        <li class="clickable">Contact Us</li>
+    </ul>
+    <p id="account-button" class="clickable">Account</p>
+    <img id="account-icon" src="/static/img/icons/account.svg"/>
+</nav>
