b273ae0eab9a418571e64db61e76d0b45242fc96
J4CKINS <electronicjack4@gmail.com>
2022-11-05 00:25:41 +0000

    Created account menu

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,11 +1,33 @@
-let mobile_nav = document.getElementById("mobile-nav");
+let mobileNav = document.getElementById("mobile-nav");
+let accountMenu = document.getElementById("account-menu")
 
 let toggleMobileNav = () => {
+    
+    // Hide the account menu if it is already open
+    if (accountMenu.style.display !== "none") {
+        accountMenu.style.display = "none";
+    }
+
     // If nav is hiding, show it
-    if (mobile_nav.style.display === "none") {
-        mobile_nav.style.display = "flex";
+    if (mobileNav.style.display === "none") {
+        mobileNav.style.display = "flex";
     }else {
         // Hide nav
-        mobile_nav.style.display = "none";
+        mobileNav.style.display = "none";
+    }
+}
+
+let toggleAccountMenu = () => {
+    // Hide the mobile nav if it is already open
+    if (mobileNav.style.display !== "none") {
+        mobileNav.style.display = "none";
+    }
+
+    // If menu is hiding, show it
+    if (accountMenu.style.display === "none") {
+        accountMenu.style.display = "flex";
+    }else {
+        // Hide nav
+        accountMenu.style.display = "none";
     }
 }
--- templates/nav.php
+++ templates/nav.php
@@ -8,7 +8,7 @@
         </ul>
         <span>
         <img id="nav-menu-icon" src="/static/img/icons/nav_menu_white.svg" onclick="toggleMobileNav();"/>
-        <img id="account-icon" src="/static/img/icons/account_icon_white.svg"/>
+        <img id="account-icon" src="/static/img/icons/account_icon_white.svg" onclick="toggleAccountMenu();"/>
         </span>
     </nav>
     <div id="mobile-nav">
@@ -16,6 +16,14 @@
         <a>Hotels</a>
         <a>Contact Us</a>
         <img src="/static/img/icons/expand_less_white.svg" onclick="toggleMobileNav();"/>
+    </div>
+    <div id="account-menu-container">
+    <div id="account-menu">
+        <a>Account Information</a>
+        <a>View Bookings</a>
+        <a>Logout</a>
+        <img src="/static/img/icons/expand_less_white.svg" onclick="toggleAccountMenu();"/>
+    </div>
     </div>
     <script src="/static/js/nav.js"></script>
 </div>
