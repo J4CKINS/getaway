9169a2f3fb07b3721633ce29ee7afea8e30e8594
J4CKINS <electronicjack4@gmail.com>
2022-11-07 12:26:36 +0000

    Account menu conditional display

--- templates/nav.php
+++ templates/nav.php
@@ -1,3 +1,7 @@
+<?php
+require_once(__DIR__ . "/../includes/session.php");
+require_once(__DIR__ . "/../includes/mysql_tools.php")
+?>
 <div class="nav-container">
     <nav>
         <p id="title" class="clickable">Getaway</p>
@@ -19,9 +23,14 @@
     </div>
     <div id="account-menu-container">
     <div id="account-menu">
-        <a href="/account/">Account Information</a>
-        <a>View Bookings</a>
-        <a href="/logout.php">Logout</a>
+        <?php if(isset($_SESSION["customerID"]) and customerAccountExists($_SESSION["customerID"])): ?>
+            <a href="/account/">Account Information</a>
+            <a>View Bookings</a>
+            <a href="/logout.php">Logout</a>
+        <?php else: ?>
+            <a href="/login.php">Login</a>
+            <a href="/signup.php">Sign up</a>
+        <?php endif; ?>
         <img src="/static/img/icons/expand_less_white.svg" onclick="toggleAccountMenu();"/>
     </div>
     </div>
