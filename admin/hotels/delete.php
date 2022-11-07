854534ee07db6d1f48018cfd16daa806c8e59440
J4CKINS <electronicjack4@gmail.com>
2022-11-07 22:44:50 +0000

    Delete script now only accepts post methods

--- admin/hotels/delete.php
+++ admin/hotels/delete.php
@@ -2,7 +2,7 @@
 include __DIR__ . "/../../includes/session.php";
 include __DIR__ . "/../../includes/mysql_tools.php";
 
-if (isset($_SERVER["REQUEST_METHOD"])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (adminLoggedIn() and adminAccountExists($_SESSION["adminID"])) {
         if (isset($_POST["hotelID"]) and hotelExists($_POST["hotelID"])) {
             deleteHotel($_POST["hotelID"]);
