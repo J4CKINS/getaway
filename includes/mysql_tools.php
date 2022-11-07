7299d9b3922fe9a76ed5a1468b26c2ead9f6fe85
J4CKINS <electronicjack4@gmail.com>
2022-11-07 10:25:10 +0000

    Added function to search for customer data

--- includes/mysql_tools.php
+++ includes/mysql_tools.php
@@ -12,4 +12,30 @@ function database_connect() {
     }
     return $conn;
 }
+
+function fetchCustomerData($ID=null, $email=null) {
+    $conn = database_connect(); // Connect to the database
+    
+    // Search by ID
+    if ($ID) {
+        $query = "SELECT * FROM `Customer` WHERE `ID` = $ID;";
+        $results = $conn->query($query);
+        
+        // Return null if no results are found
+        if ($results->num_rows < 1) { return null; }
+
+        return $results->fetch_assoc();
+    }
+
+    // Search by Email
+    elseif ($email) {
+        $query = "SELECT * FROM `Customer` WHERE `Email` = '$email';";
+        $results = $conn->query($query);
+
+        // Return null if no results are found
+        if ($results->num_rows < 1) { return null; }
+
+        return $results->fetch_assoc();
+    }
+}  
 ?>
