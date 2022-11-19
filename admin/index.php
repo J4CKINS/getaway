629044c3cf2d895110f8908e10b0668d79286e2b
J4CKINS <electronicjack4@gmail.com>
2022-11-19 01:09:11 +0000

    Created admin home page

---
+++ admin/index.php
@@ -0,0 +1,31 @@
+<?php
+include __DIR__ . "/../includes/auth_admin.php";
+
+if (!authAdmin()) {
+    header("Location: /admin/login.php");
+    return;
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Getaway - Admin</title>
+        <?php include __DIR__ . "/../templates/head.php"; ?>
+        <link rel="stylesheet" type="text/css" href="/static/css/admin/admin.css">
+    </head>
+    <body>
+        <?php include __DIR__ . "/../templates/nav.php"; ?>
+        <div class="wrapper">
+            <section>
+                <h1>Getaway</h1>
+                <h2>Admin Panel</h2>
+            </section>
+            <section>
+                <h3><a href="hotels/">Hotel Manager</a></h3>
+                <h3><a href="gallery/">Gallery Manager</a></h3>
+                <h3><a href="logout.php">Logout</a></h3>
+            </section>
+        </div>
+        <?php include __DIR__ . "/../templates/footer.php"; ?>
+    </body>
+</html>
