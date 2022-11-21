c1264e8da3be78e1726d451c40410df99ec61f25
J4CKINS <electronicjack4@gmail.com>
2022-11-21 00:30:58 +0000

    Hotel gallery is now cleared once hotel is deleted

--- admin/hotels/delete.php
+++ admin/hotels/delete.php
@@ -5,7 +5,21 @@ include __DIR__ . "/../../includes/auth_admin.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (authAdmin()) {
         if (isset($_POST["hotelID"]) and hotelExists($_POST["hotelID"])) {
-            deleteHotel($_POST["hotelID"]);
+
+            // Clear the hotel gallery
+            $uploadsFolderPath = __DIR__ . "/../..";
+            $imageFiles = fetchHotelImages($_POST["hotelID"]);
+            
+            // Remove gallery entries from database
+            clearHotelGallery($_POST["hotelID"]);
+            
+            // Delete image files
+            foreach($imageFiles as $image) {
+            unlink($uploadsFolderPath . $image["ImageURL"]);
+            }
+
+            deleteHotel($_POST["hotelID"]); // Remove hotel from database
+
             header("Location: /admin/hotels/");
             return;
         }
